318c960a1241f653b5d8f48bac7380187a609a92
Karn Jaroenporn <karnjj@hotmail.com>
2022-05-01 19:30:49 +0700

    feat: add cpp ref link

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ app.use("/reference", express.static("reference"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(
+    `You can access cpp reference in http://localhost:${PORT}/reference/en`
+  );
 });
